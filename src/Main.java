6fdeed01bab22af25ef5d95aa7ace7aeb6d21a49
Minesh <54360467+mineshgaikar50@users.noreply.github.com>
2024-09-04 22:29:27 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.driver.Product;
+class Main {
+
+
+    public static void main(String[] args) {
+        
+        Product p = new Product();
+       
+        int result1 = p.product(3, 4);
+        int result2 = p.product(3, 4, 5);
+        double result3 = p.product(2.5, 4.0);
+       
+        System.out.println("Result of product(3, 4): " + result1);
+        System.out.println("Result of product(3, 4, 5): " + result2);
+        System.out.println("Result of product(2.5, 4.0): " + result3);
+    }
+}
